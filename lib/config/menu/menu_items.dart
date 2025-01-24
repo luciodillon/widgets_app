ec3b00b75aaece67ecbd7cc5e9a3a5a9d5e3f087
Lucio Dillon <lucio.dillon@globant.com>
2025-01-24 10:54:42 -0300

    corregi nombre

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -13,7 +13,7 @@ class MenuItem {
     required this.icon});
 }
 
-const appMenuItmes = <MenuItem>[
+const appMenuItems = <MenuItem>[
   MenuItem(
    title: 'Botones',
    subTitle: 'Varios botones en Flutter',
