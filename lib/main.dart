f4d27b2ff7b6009dead90ab22c65f9838609d5b5
Lucio Dillon <lucio.dillon@globant.com>
2025-02-03 09:07:08 -0300

    backup

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Widgets',
+      title: 'Flutter Widge',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
 
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+      'Busca la comida',
+      'Irure aute ex ullamco culpa id deserunt laboris nulla ad voluptate eu occaecat laborum.',
+      'assets/Images/1.png'),
+  SlideInfo(
+      'Entrega rapida',
+      'Eu ex tempor Lorem cupidatat anim commodo labore anim reprehenderit mollit ullamco irure laboris deserunt.',
+      'assets/Images/2.png'),
+  SlideInfo(
+      'Disfurta la comida la comida',
+      'Excepteur deserunt exercitation deserunt tempor sit aliquip id consectetur excepteur sint ea eu cillum.',
+      'assets/Images/3.png'),
+];
+
+class AppTutorialScreen extends StatelessWidget {
   static const String name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
@@ -9,9 +31,29 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tutorial'),
-      ),
-    );
+        body: PageView(
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map(
+                  (slideData) => _Slide(
+                      title: slideData.title,
+                      caption: slideData.caption,
+                      imageUrl: slideData.imageUrl),
+                )
+                .toList()));
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide(
+      {required this.title, required this.caption, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
