d663be9451eded7d1417b496f93adb41a1a4ee83
Lucio Dillon <lucio.dillon@globant.com>
2025-02-04 12:10:55 -0300

    infite scroll start

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class InfiniteScrollScreen extends StatelessWidget {
-
   static const String name = 'infinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: ListView.builder(
+      itemBuilder: (context, index) {
+        return FadeInImage(
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: 300,
+          placeholder: AssetImage('assets/Images/jar-loading.gif'),
+          image: NetworkImage('https://picsum.photos/id/1/500/300')
+        );
+      },
+    ));
   }
 }
