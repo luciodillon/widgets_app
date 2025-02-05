577cb0e260c98d63368deffde4e29666b9c2d4be
Lucio Dillon <lucio.dillon@globant.com>
2025-02-05 11:47:52 -0300

    infite scroll terminado

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -11,43 +12,85 @@ class InfiniteScrollScreen extends StatefulWidget {
 }
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
-
   List<int> imagesIds = [1, 2, 3, 4, 5];
   final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
 
   @override
   void initState() {
     super.initState();
 
     scrollController.addListener(() {
-
-      if ((scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent) {
-        //load next page
-        addFiveImages();
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPage();
       }
-      
-    }); 
-    
+    });
   }
 
   @override
   void dispose() {
     scrollController.dispose();
-
+    isMounted = false;
     super.dispose();
   }
-
 
   void addFiveImages() {
     final lastId = imagesIds.last;
 
     imagesIds.addAll(
-      [1,2,3,4,5].map((e) => lastId + e)
+      [1,2,3,4,5].map((e) => lastId + e),
     );
 
     setState(() {
       
     });
+  }
+
+  Future loadNextPage() async {
+
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration (seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    if(!isMounted) return;
+
+    setState(() {
+      
+    });
+    moveScrollToBottom();
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+
+    await Future.delayed(const Duration(seconds: 3));
+    if(!isMounted) return; 
+
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+
+    setState(() {});
+  }
+
+  moveScrollToBottom() {
+    if(scrollController.position.pixels + 150 >= scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 120,
+     duration: Duration(milliseconds: 300), 
+     curve: Curves.fastOutSlowIn
+     );
   }
 
   @override
@@ -58,27 +101,31 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: 
-        ListView.builder(
-          controller: scrollController,
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: 300,
-              placeholder: AssetImage('assets/Images/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300')
-          );
-        },
-            ),
+        child: RefreshIndicator(
+          edgeOffset: 15,
+          onRefresh: onRefresh,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                  placeholder: AssetImage('assets/Images/jar-loading.gif'),
+                  image: NetworkImage(
+                      'https://picsum.photos/id/${imagesIds[index]}/500/300'));
+            },
+          ),
+        ),
       ),
-
-    floatingActionButton: FloatingActionButton(
-      onPressed: () => context.pop(),
-      child: Icon(Icons.arrow_back_ios),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: isLoading ? SpinPerfect(
+          infinite: true,
+          child: const Icon(Icons.refresh_outlined)
+        ): Icon(Icons.arrow_back_ios)
       ),
-    
-  );
+    );
   }
 }
