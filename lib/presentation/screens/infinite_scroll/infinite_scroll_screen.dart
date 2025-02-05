c212c350285795f01c2cec35730db82235160a73
Lucio Dillon <lucio.dillon@globant.com>
2025-02-05 10:22:25 -0300

    inifinite scroll

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,22 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const String name = 'infinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+
+      if ((scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent) {
+        //load next page
+        addFiveImages();
+      }
+      
+    }); 
+    
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+
+    super.dispose();
+  }
+
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+
+    imagesIds.addAll(
+      [1,2,3,4,5].map((e) => lastId + e)
+    );
+
+    setState(() {
+      
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ListView.builder(
-      itemBuilder: (context, index) {
-        return FadeInImage(
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: 300,
-          placeholder: AssetImage('assets/Images/jar-loading.gif'),
-          image: NetworkImage('https://picsum.photos/id/1/500/300')
-        );
-      },
-    ));
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: 
+        ListView.builder(
+          controller: scrollController,
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: AssetImage('assets/Images/jar-loading.gif'),
+              image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300')
+          );
+        },
+            ),
+      ),
+
+    floatingActionButton: FloatingActionButton(
+      onPressed: () => context.pop(),
+      child: Icon(Icons.arrow_back_ios),
+      ),
+    
+  );
   }
 }
